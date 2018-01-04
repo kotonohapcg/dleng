a33856ad0e4a336e75534f8ef7c60814123af9d1
kotonohapcg <tukudatktk06@gmail.com>
2018-01-05 06:34:31 +0900

    スニペット例

---
+++ hoge.d
@@ -0,0 +1,24 @@
+import std.stdio;
+
+//d.jsonにこう記述
+/*
+"for rep":{
+		"prefix": "REP",
+		"body": [
+			"for(int ($1)=0;($1)<($2);($1)++{",
+				"//nothing",
+			"}"
+		],
+		"description": "REP macro fo for-sentence"
+	},
+*/
+
+//実際に呼び出すとき
+
+REP//ここでTabまたはC-Space
+
+for(int ()=0;()<();()++{
+//nothing
+}
+
+//っていう感じで出る
