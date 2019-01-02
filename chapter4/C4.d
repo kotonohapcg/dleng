66a74ebf733e504bc1d4ef6b8fa52a2309c703e6
kotonohapcg <tukudatktk06@gmail.com>
2019-01-02 16:51:35 +0900

    delete C4 exe and, add length assertion

--- chapter4/C4.d
+++ chapter4/C4.d
@@ -11,133 +11,25 @@ void main() {
     foreach(i; 0 .. array.length) {
         array[i] = uniform(0.0, 1.0);
     }
-
-    /*
-    //use ref id -> 要素を直接参照
-    auto array = new double[uniform(1, 128)];
-    foreach(ref element; 0 .. array.length) {
-        element = uniform(0.0, 1.0);
-    }
-    */
-
-    foreach(value; 0 .. array.length) {
-        writeln(value);
-    }
+    
+    writeln(array1.length);
+    writeln(arr.length);
+    writeln(array.length);
+    assert(array1.length == 20);
+    assert(arr.length == 20);
+    assert(array.length == 20);
 
 }
 
 /*
-output exapmle::
+output example::
 
 kotonoha@LAPTOP-FBRM3VSM:/mnt/c/Users/tukud/dleng/chapter4$ ./C4
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
 20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
-96
-97
-98
-99
-100
-101
-102
-103
-104
-105
-106
-107
-108
-109
+20
+47  //length change every
+core.exception.AssertError@C4.d(20): Assertion failure
+----------------
+??:? _d_assertp [0xc62684e1]
+??:? _Dmain [0xc6256863]
 */
