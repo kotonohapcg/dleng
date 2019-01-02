2069339566fc09cfbdd5d36dc3da9baab5762c84
kotonohapcg <tukudatktk06@gmail.com>
2019-01-02 15:44:02 +0900

    add

---
+++ chapter1/binarySearch.d
@@ -0,0 +1,20 @@
+bool binarySearch(T) (T[] input, T value) {
+    while(!input.empty) {
+        auto i = input.length / 2;
+        auto mid = input[i];
+
+        if(mid > value) {
+            input = input[0 .. i];
+        } else if(mid < value) {
+            input = input[i + 1 .. $];
+        } else {
+            return value;
+        }
+    }
+    return false;
+}
+
+unittest {
+    assert(binarySearch([1,3,6,7,9,15], 6));
+    assert(binarySearch([1,3,6,7,9,15], 5));
+}
